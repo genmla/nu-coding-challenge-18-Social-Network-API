1e43664012d5789894fa80e02deb8a8b5c91d849
genmla <gmmladenka@gmail.com>
2023-02-11 14:37:16 -0600

    built get, put, and delete user routes for CRUD w/ id

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,9 +2,18 @@ const router = require('express').Router();
 const {
     getUsers,
     createUser,
+    getSingleUser,
+    updateUser, 
+    deleteUser,
 } = require('../../controllers/userControllers');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
